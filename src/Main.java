90ac5e013374d7d51b8c5dd146c24c42358b7a20
Mjollnir94 <marcel_hollink@hotmail.com>
2015-04-20 14:38:58 +0200

    new fractions nothing more

--- src/Main.java
+++ src/Main.java
@@ -7,19 +7,22 @@ public class Main {
     public static void main(String[] args) {
 
         // Create 2 Fractions
-        Fraction a = new Fraction(1, 1,3);
-        Fraction b = new Fraction(1, -4, 3);
+        Fraction a = new Fraction(1,3);
+        Fraction b = new Fraction(0, -4, 3);
+        Fraction c = new Fraction(18, -4, 3);
+        Fraction d = new Fraction(12, 9, 83);
+        Fraction e = new Fraction(2, 11, 22);
 
         // Add fraction A to B
-        System.out.println("("+a +") + ("+ b + ") = " + Calculator.add(a,b));
+        System.out.println("("+ a +") + ("+ b + ") = " + Calculator.add(a,b));
 
-        // Subtract fraction A to B
-        System.out.println("("+a +") - ("+ b + ") = " + Calculator.subtract(a,b));
+        // Subtract fraction A to C
+        System.out.println("("+ a +") - ("+ c + ") = " + Calculator.subtract(a,c));
 
         // Multiply fraction A to B
-        System.out.println("("+a +") * ("+ b + ") = " + Calculator.multiply(a,b));
+        System.out.println("("+ c +") * ("+ d + ") = " + Calculator.multiply(c,d));
 
         // Divide fraction A to B
-        System.out.println("("+a +") / ("+ b + ") = " + Calculator.divide(a,b));
+        System.out.println("("+ e +") / ("+ b + ") = " + Calculator.divide(e,b));
     }
 }
